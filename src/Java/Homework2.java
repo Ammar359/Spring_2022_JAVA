cbf7a3983cdb4b940b6b11f9f4b19da4d35b1806
Ammar Ashraf <umaar4u@hotmail.com>
2022-03-07 23:03:50 -0500

    Homework2

---
+++ src/Java/Homework2.java
@@ -0,0 +1,36 @@
+public class Homework2 {
+    public static void main(String[] args) {
+
+        byte numberOfHoursInDay = 24;
+        System.out.println("Number of hours in a day --> "+ numberOfHoursInDay);
+
+        int maxNumberOfDaysInYear = 365;
+        System.out.println("Number of days in a year = " + maxNumberOfDaysInYear);
+
+        int totalNumberOfEmployees = 190;
+        System.out.println("There are "+totalNumberOfEmployees+" employees in this organization");
+
+        long populationOfCountry = 2199847;
+        System.out.println("Population of this country is = " + populationOfCountry);
+
+        float mortgageRate = 3.5f;
+        System.out.println("bank is offering "+mortgageRate+" to its customers");
+
+        double balanceInMyBankAccount = 212908.1;
+        System.out.println("Balance of my bank account is " + balanceInMyBankAccount );
+
+        boolean doesSunRiseFromTheWest =  false;
+        System.out.println("Does sun rises from the West = " + doesSunRiseFromTheWest);
+
+        char initialsOfMyName = 'A' ;
+        char InitialOfMyName = 'A';
+        System.out.print("Initials of my name are ="+ initialsOfMyName + initialsOfMyName);
+
+
+        String storeYourFullName = "Ammar Ashraf";
+        System.out.println("\n My full name is "+ storeYourFullName);
+
+
+    }
+
+}
